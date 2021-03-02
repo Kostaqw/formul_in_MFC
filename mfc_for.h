01018316445904e89ab3ab52b943db37847ab9b2
Kostaqw <74700605+Kostaqw@users.noreply.github.com>
2021-03-02 23:36:56 +0300

    Add files via upload

---
+++ mfc_for.h
@@ -0,0 +1,32 @@
+﻿
+// mfc_for.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CmfcforApp:
+// Сведения о реализации этого класса: mfc_for.cpp
+//
+
+class CmfcforApp : public CWinApp
+{
+public:
+	CmfcforApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmfcforApp theApp;
